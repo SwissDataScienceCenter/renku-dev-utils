d7bdfd863f885e8f3f3d6883e4f52b644992f8b0
Wes Johnson <wes@wes-johnson.com>
2025-12-04 14:23:28 +0100

    restore original kubeconfig loading functionality

--- pkg/k8s/client.go
+++ pkg/k8s/client.go
@@ -2,26 +2,43 @@ package k8s
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
 func getConfig() (*rest.Config, error) {
+	// Try in-cluster config first (for running inside K8s pods)
 	config, err := rest.InClusterConfig()
 	if err == nil {
 		return config, nil
 	}
 
-	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
-	configOverrides := &clientcmd.ConfigOverrides{}
-	kubeConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, configOverrides)
+	// Check KUBECONFIG environment variable
+	kubeconfigPath := os.Getenv("KUBECONFIG")
+	if kubeconfigPath != "" {
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+		if err != nil {
+			return nil, err
+		}
+		return config, nil
+	}
 
-	config, err = kubeConfig.ClientConfig()
+	// Fall back to default kubeconfig location
+	home := homedir.HomeDir()
+	if home == "" {
+		return nil, fmt.Errorf("could not determine home directory")
+	}
+
+	kubeconfigPath = filepath.Join(home, ".kube", "config")
+	config, err = clientcmd.BuildConfigFromFlags("", kubeconfigPath)
 	if err != nil {
-		return nil, fmt.Errorf("failed to load kubeconfig: %w", err)
+		return nil, err
 	}
 
 	return config, nil
